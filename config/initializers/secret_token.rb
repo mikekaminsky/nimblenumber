532b119a4e283cde536a5330f78d047f140a460f
mikekaminsky <mkaminsky@casecommons.org>
2014-03-07 11:02:08 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nimblenumber::Application.config.secret_key_base = '7cb889dd0c4a64ec51caff262fb88e7caf150771728c18de0313b5e976d602b54b53a6297732a2b814b279d395e14d75166415b202e7420c764f29905cbe6313'
