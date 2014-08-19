0e336277aa1a5fe31ef5cdf1876fbe537f7ad4dd
Michael <ccUser@cc-notebook-19.home>
2014-08-18 21:08:26 -0400

    Adding pages controller and an 'about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Nimblenumber::Application.routes.draw do
 
+  get "pages/home"
+  get "pages/help"
   get "welcome/index"
   root 'welcome#index'
 
   get '/', to: 'welcome#index', as: :home
+  get '/about', to: 'pages#about'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
